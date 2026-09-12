b29ef11d3439b6212a77ed1d49a9838d0832791a
jaykk99 <jayomer1234@gmail.com>
2026-09-12 18:06:31 -0500

    I sent u the wallet address btc bro

--- src/types/ServerlessFunction.ts
+++ src/types/ServerlessFunction.ts
@@ -1,6 +1,7 @@
 export interface ServerlessFunction {
   id: string;
   name: string;
+  route: string; // e.g. "/api/my-function"
   code: string;
   created_at: string;
   version?: string; // Add version to the interface
